e62068b6c2416a8b273aaf5df33479a0dbf06928
danilojpfreitas <danilojpfreitas@gmail.com>
2023-01-12 18:17:18 -0300

    Deploy

--- shared/database.py
+++ shared/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+
+SQLALCHEMY_DATABASE_URL = os.getenv("URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
