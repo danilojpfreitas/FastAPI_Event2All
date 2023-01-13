1a0b92164abc1ddbfa260353be48f9012b45270c
danilojpfreitas <danilojpfreitas@gmail.com>
2023-01-13 16:46:35 -0300

    deploy

---
+++ alembic/versions/1a64d72d79f1_01.py
@@ -0,0 +1,24 @@
+"""01
+
+Revision ID: 1a64d72d79f1
+Revises: 
+Create Date: 2023-01-13 12:07:41.935200
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1a64d72d79f1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
